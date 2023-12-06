ff707895da5269bdb8e8442cf303b96ced9687cd
Tulio Natale <tulio.natale@sab.sachsen.com>
2023-12-06 08:29:00 +0100

    same as the proffesor

--- src/app/books/book-search/book-search.component.ts
+++ src/app/books/book-search/book-search.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-book-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './book-search.component.html',
-  styleUrl: './book-search.component.scss'
+  styleUrl: './book-search.component.scss',
 })
 export class BookSearchComponent {
+  searchControl = new FormControl('', { nonNullable: true });
 
+  constructor() {
+    this.searchControl.valueChanges.subscribe((e) => {
+      console.log(e);
+    });
+  }
 }
